4444f6f318c6bbcaa865bd2be545aa47c527e8ce
Maria-Lavinia Otelea <mlo@frankly.dk>
2023-05-02 09:12:32 +0200

    removing suggestion modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { SuggestionsModule } from './suggestions/suggestions.module';
+/* `import { SuggestionsModule } from './suggestions/suggestions.module';` is importing the
+`SuggestionsModule` from the `./suggestions/suggestions.module` file. This module is likely defining
+controllers, services, and providers related to suggestions in the application. By importing this
+module in the `AppModule`, the controllers, services, and providers defined in the
+`SuggestionsModule` can be used in the application. */
+// import { SuggestionsModule } from './suggestions/suggestions.module';
 import { IssuesModule } from './issues/issues.module';
 import { InfosModule } from './infos/infos.module';
 import { RegionsModule } from './regions/regions.module';
 import { ApartmentInfoModule } from './apartment-info/apartment-info.module';
 import { UsersModule } from './users/users.module';
-import { Suggestion } from './suggestions/entities/suggestion.entity';
+// import { Suggestion } from './suggestions/entities/suggestion.entity';
 import { Issue } from './issues/entities/issue.entity';
 import { Info } from './infos/entities/info.entity';
 import { Region } from './regions/entities/region.entity';
@@ -31,12 +36,11 @@ import { CategoriesModule } from './categories/categories.module';
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         autoLoadEntities: true,
-        entities: [Suggestion, Issue, Info, Region, ApartmentInfo, User],
+        entities: [ Issue, Info, Region, ApartmentInfo, User],
         synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
       }),
       inject: [ConfigService],
     }),
-    SuggestionsModule,
     IssuesModule,
     InfosModule,
     RegionsModule,
