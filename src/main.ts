c820e0ab0e3480892413c02a7bb9dea0aca5a8ce
Adelina Radulescu <adelina.radulescu8@yahoo.com>
2023-05-16 17:10:58 +0200

    added cors enable

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
+// import { CorsOptions } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(),
+  );
+
+  // Enable CORS
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
